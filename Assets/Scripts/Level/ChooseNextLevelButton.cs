75e3c9e7d3fefedd9288fe25a3c5f5463747ae9d
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-11-18 23:22:43 +0800

    Добавлен выбор следующего уровня при победе и возвращение в главное меню на моследнем уровне

---
+++ Assets/Scripts/Level/ChooseNextLevelButton.cs
@@ -0,0 +1,17 @@
+namespace Level
+{
+    using Common;
+    using Reflex.Attributes;
+
+    internal class ChooseNextLevelButton : AbstractButton
+    {
+        private Level _level;
+
+        [Inject]
+        private void Initialize(Level level) =>
+            _level = level;
+        
+        protected override void HandleClick() =>
+            _level.Choose(_level.Chosen + 1);
+    }
+}
