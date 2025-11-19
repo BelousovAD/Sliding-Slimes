ee6e7ebc43dc531e30408d29be11fa382c732a47
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-11-19 22:12:18 +0800

    Добавлена и настроена кнопка добавления времени при проигрыше

---
+++ Assets/Scripts/Timer/AddTimeButton.cs
@@ -0,0 +1,20 @@
+namespace Timer
+{
+    using Common;
+    using Reflex.Attributes;
+    using UnityEngine;
+
+    internal class AddTimeButton : AbstractButton
+    {
+        [SerializeField, Min(1)] private int _amount;
+
+        private CoroutineTimer _timer;
+
+        [Inject]
+        private void Initialize(CoroutineTimer timer) =>
+            _timer = timer;
+        
+        protected override void HandleClick() =>
+            _timer.Add(_amount);
+    }
+}
