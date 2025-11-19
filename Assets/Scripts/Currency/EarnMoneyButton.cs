a3e042c3e3883f59a94470686385605d7e557682
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-11-19 18:36:14 +0800

    Настроен заработок монет при победе и просмотре видео

--- Assets/Scripts/Currency/EarnMoneyButton.cs
+++ Assets/Scripts/Currency/EarnMoneyButton.cs
@@ -1,5 +1,6 @@
 namespace Currency
 {
+    using Bootstrap;
     using Common;
     using Reflex.Attributes;
     using UnityEngine;
@@ -7,14 +8,31 @@ namespace Currency
     internal class EarnMoneyButton : AbstractButton
     {
         [SerializeField, Min(0)] private int _amount;
+        [SerializeField] private bool _isRewardedAd;
 
         private Money _money;
+        private SavvyServicesProvider _services;
 
         [Inject]
-        private void Initialize(Money money) =>
+        private void Initialize(Money money, SavvyServicesProvider servicesProvider)
+        {
             _money = money;
-        
-        protected override void HandleClick() =>
+            _services = servicesProvider;
+        }
+
+        protected override void HandleClick()
+        {
+            if (_isRewardedAd)
+            {
+                _services.Mediation.ShowRewardedAd(Earn);
+            }
+            else
+            {
+                Earn();
+            }
+        }
+
+        private void Earn() =>
             _money.Earn(_amount);
     }
 }
