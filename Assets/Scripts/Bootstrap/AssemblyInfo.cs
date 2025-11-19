cb1cb4e0288709294d09ed8f5905059200629354
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-11-19 14:40:01 +0800

    Добавлено отображение монет

--- Assets/Scripts/Bootstrap/AssemblyInfo.cs
+++ Assets/Scripts/Bootstrap/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Audio")]
+[assembly: InternalsVisibleTo("Currency")]
 [assembly: InternalsVisibleTo("Level")]
 [assembly: InternalsVisibleTo("Timer")]
---
+++ Assets/Scripts/Currency/MoneyTMPView.cs
@@ -0,0 +1,35 @@
+namespace Currency
+{
+    using Reflex.Attributes;
+    using Savvy.Extensions;
+    using TMPro;
+    using UnityEngine;
+
+    [RequireComponent(typeof(TMP_Text))]
+    public class MoneyTMPView : MonoBehaviour
+    {
+        [SerializeField] private string _format = "{0}";
+        
+        private TMP_Text _textField;
+        private Money _money;
+
+        [Inject]
+        private void Initialize(Money money) =>
+            _money = money;
+
+        private void Awake() =>
+            _textField = GetComponent<TMP_Text>();
+
+        private void OnEnable()
+        {
+            _money.Changed += UpdateView;
+            UpdateView();
+        }
+
+        private void OnDisable() =>
+            _money.Changed -= UpdateView;
+
+        private void UpdateView() =>
+            _textField.text = string.Format(_format, _money.Value.ToNumsFormat());
+    }
+}
