71981b562079c654ff5ecbb795720f21cfd4d7e6
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-11-20 18:43:29 +0800

    Добавлен установщик TimeScale

---
+++ Assets/Scripts/Gameplay/TimeScaleSetter.cs
@@ -0,0 +1,28 @@
+namespace Gameplay
+{
+    using MirraGames.SDK;
+    using UnityEngine;
+
+    public class TimeScaleSetter : MonoBehaviour
+    {
+        [SerializeField, Min(0f)] private float _timeScale;
+        [SerializeField] private bool _setOnEnable;
+        [SerializeField] private bool _setOnDisable;
+
+        private void OnEnable()
+        {
+            if (_setOnEnable)
+            {
+                MirraSDK.Time.Scale = _timeScale;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_setOnDisable)
+            {
+                MirraSDK.Time.Scale = _timeScale;
+            }
+        }
+    }
+}
