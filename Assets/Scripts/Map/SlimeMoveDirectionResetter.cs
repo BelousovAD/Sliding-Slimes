4b5e7f4a23aa21ae1dc141c81f78620635cf0866
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-11-27 20:50:08 +0800

    Добавлен сброс направления движения слайма при касании триггера портала

--- Assets/Scripts/Map/SlimeMoveDirectionResetter.cs
+++ Assets/Scripts/Map/SlimeMoveDirectionResetter.cs
@@ -2,6 +2,7 @@ namespace Map
 {
     using UnityEngine;
 
+    [RequireComponent(typeof(Collider))]
     internal class SlimeMoveDirectionResetter : MonoBehaviour
     {
         private const float CollinearDot = 1f;
@@ -12,6 +13,14 @@ namespace Map
                 Mathf.Approximately(
                     Vector3.Dot(slimeMover.Direction, other.contacts[0].normal),
                     CollinearDot))
+            {
+                slimeMover.Move(Vector2Int.zero);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.TryGetComponent(out SlimeMover slimeMover))
             {
                 slimeMover.Move(Vector2Int.zero);
             }
