0449adeaa5dc7a5d02e361561cfca279b2b38c27
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-11-27 19:21:46 +0800

    Создан сбрасыватель направления передвижения слайма

---
+++ Assets/Scripts/Map/SlimeMoveDirectionResetter.cs
@@ -0,0 +1,20 @@
+namespace Map
+{
+    using UnityEngine;
+
+    public class SlimeMoveDirectionResetter : MonoBehaviour
+    {
+        private const float CollinearDot = 1f;
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            if (other.gameObject.TryGetComponent(out SlimeMover slimeMover) &&
+                Mathf.Approximately(
+                    Vector3.Dot(slimeMover.Direction, other.contacts[0].normal),
+                    CollinearDot))
+            {
+                slimeMover.Move(Vector2Int.zero);
+            }
+        }
+    }
+}
