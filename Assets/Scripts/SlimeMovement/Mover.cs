e2bb076ca1cc0af5011afaa545dd4d0c2ac3414e
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-12-08 16:54:55 +0800

    Убрана возможность передвигать несколько слаймов одновременно

--- Assets/Scripts/SlimeMovement/Mover.cs
+++ Assets/Scripts/SlimeMovement/Mover.cs
@@ -6,7 +6,8 @@ namespace SlimeMovement
     internal class Mover : MonoBehaviour
     {
         [SerializeField, Min(0f)] private float _speed;
-        
+
+        private static Mover _busyInstance;
         private Rigidbody _rigidbody;
         
         public Vector3 Direction { get; private set; }
@@ -16,14 +17,21 @@ namespace SlimeMovement
 
         public void Move(Vector2Int direction)
         {
+            if (_busyInstance is not null && _busyInstance != this)
+            {
+                return;
+            }
+            
             Direction = new Vector3(direction.x, 0, direction.y);
             _rigidbody.isKinematic = false;
             transform.localPosition = Vector3Int.RoundToInt(transform.localPosition);
             _rigidbody.velocity = Direction * _speed;
-            
+            _busyInstance = this;
+
             if (direction == Vector2Int.zero)
             {
                 _rigidbody.isKinematic = true;
+                _busyInstance = null;
             }
         }
     }
