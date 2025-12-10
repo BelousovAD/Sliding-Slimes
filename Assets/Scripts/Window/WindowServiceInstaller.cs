0ba750dc0c3266b49a3063266aa8f118feac7576
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-12-10 19:47:45 +0800

    Добавлены горизонтальные окна

--- Assets/Scripts/Window/WindowServiceInstaller.cs
+++ Assets/Scripts/Window/WindowServiceInstaller.cs
@@ -1,21 +1,33 @@
 namespace Window
 {
+    using MirraGames.SDK;
     using Reflex.Core;
     using UnityEngine;
 
     internal class WindowServiceInstaller : MonoBehaviour, IInstaller
     {
-        [SerializeField] private MonoBehaviour _spawner;
+        [SerializeField] private MonoBehaviour _horizontalSpawner;
+        [SerializeField] private MonoBehaviour _verticalSpawner;
         
-        public void InstallBindings(ContainerBuilder builder) =>
-            builder.AddSingleton(new WindowService(_spawner as IWindowSpawner), typeof(IWindowService));
+        public void InstallBindings(ContainerBuilder builder)
+        {
+            builder.AddSingleton(
+                new WindowService((MirraSDK.Device.IsMobile ? _verticalSpawner : _horizontalSpawner) as IWindowSpawner),
+                typeof(IWindowService));
+        }
 
         private void OnValidate()
         {
-            if (_spawner is not null && _spawner is not IWindowSpawner)
+            if (_horizontalSpawner is not null && _horizontalSpawner is not IWindowSpawner)
             {
-                Debug.LogError($"{nameof(_spawner)} must inherited {nameof(IWindowSpawner)}");
-                _spawner = null;
+                Debug.LogError($"{nameof(_horizontalSpawner)} must inherited {nameof(IWindowSpawner)}");
+                _horizontalSpawner = null;
+            }
+            
+            if (_verticalSpawner is not null && _verticalSpawner is not IWindowSpawner)
+            {
+                Debug.LogError($"{nameof(_verticalSpawner)} must inherited {nameof(IWindowSpawner)}");
+                _verticalSpawner = null;
             }
         }
     }
