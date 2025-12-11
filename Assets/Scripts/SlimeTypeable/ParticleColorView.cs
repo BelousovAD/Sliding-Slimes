373bfbe6783c196b2ee9cd9fcab3051270660fd7
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-12-11 22:00:17 +0800

    Создано отображение цвета частиц в зависимости от цвета слайма

---
+++ Assets/Scripts/SlimeTypeable/ParticleColorView.cs
@@ -0,0 +1,51 @@
+namespace SlimeTypeable
+{
+    using System;
+    using UnityEngine;
+
+    [RequireComponent(typeof(ParticleSystem))]
+    internal class ParticleColorView : MonoBehaviour
+    {
+        [SerializeField] private MonoBehaviour _slimeTypeableComponent;
+
+        private ISlimeTypeable _slimeTypeable;
+        private ParticleSystem _particleSystem;
+
+        private Color Color
+        {
+            get
+            {
+                return _slimeTypeable.Type switch
+                {
+                    SlimeType.Blue => new Color(0f, 0.5f, 1f),
+                    SlimeType.Grey => Color.grey,
+                    SlimeType.Orange => new Color(1f, 0.5f, 0f),
+                    SlimeType.Purple => Color.magenta,
+                    SlimeType.Red => Color.red,
+                    SlimeType.White => Color.white,
+                    SlimeType.Yellow => Color.yellow,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            }
+        }
+
+        private void Awake() =>
+            _particleSystem = GetComponent<ParticleSystem>();
+        
+        private void Start()
+        {
+            _slimeTypeable = _slimeTypeableComponent as ISlimeTypeable ?? throw new InvalidOperationException();
+            ParticleSystem.MainModule particleSystemMain = _particleSystem.main;
+            particleSystemMain.startColor = Color;
+        }
+
+        private void OnValidate()   
+        {
+            if (_slimeTypeableComponent is not (null or ISlimeTypeable))
+            {
+                Debug.LogError($"{nameof(_slimeTypeableComponent)} must inherit {nameof(ISlimeTypeable)}");
+                _slimeTypeableComponent = null;
+            }
+        }
+    }
+}
