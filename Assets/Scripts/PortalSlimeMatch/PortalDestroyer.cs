649e37d52ac6fe39b6f11445cb91544d7588a093
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-12-12 00:16:50 +0800

    Добавлена анимация уничтожения слайма и портала

--- Assets/Scripts/PortalSlimeMatch/PortalDestroyer.cs
+++ Assets/Scripts/PortalSlimeMatch/PortalDestroyer.cs
@@ -1,11 +1,16 @@
 namespace PortalSlimeMatch
 {
+    using DG.Tweening;
     using Model;
     using UnityEngine;
 
     internal class PortalDestroyer : MonoBehaviour
     {
         [SerializeField] private Portal _portal;
+        [SerializeField] private Transform _model;
+        [SerializeField] private ParticleSystem _particleSystem;
+        [SerializeField, Min(0.001f)] private float _animationDuration = 0.001f;
+        [SerializeField, Min(0.001f)] private float _particleSystemDuration = 0.001f;
 
         private void OnEnable() =>
             _portal.SlimeCaught += Destroy;
@@ -13,7 +18,14 @@ namespace PortalSlimeMatch
         private void OnDisable() =>
             _portal.SlimeCaught -= Destroy;
 
-        private void Destroy() =>
-            Destroy(gameObject);
+        private void Destroy()
+        {
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(_model.DOScale(Vector3.zero, _animationDuration).SetEase(Ease.Linear));
+            sequence.AppendCallback(() => _particleSystem.Play());
+            sequence.AppendInterval(_particleSystemDuration);
+            sequence.AppendCallback(() => Destroy(gameObject));
+            sequence.SetUpdate(true);
+        }
     }
 }
--- Assets/Scripts/PortalSlimeMatch/SlimeDestroyer.cs
+++ Assets/Scripts/PortalSlimeMatch/SlimeDestroyer.cs
@@ -1,19 +1,27 @@
 namespace PortalSlimeMatch
 {
+    using DG.Tweening;
     using Model;
     using UnityEngine;
 
     internal class SlimeDestroyer : MonoBehaviour
     {
         [SerializeField] private Slime _slime;
-
+        [SerializeField] private Transform _model;
+        [SerializeField, Min(0.001f)] private float _animationDuration = 0.001f;
+        
         private void OnEnable() =>
             _slime.Caught += Destroy;
 
         private void OnDisable() =>
             _slime.Caught -= Destroy;
 
-        private void Destroy() =>
-            Destroy(gameObject);
+        private void Destroy()
+        {
+            Sequence sequence = DOTween.Sequence();
+            sequence.Append(_model.DOScale(Vector3.zero, _animationDuration).SetEase(Ease.Linear));
+            sequence.AppendCallback(() => Destroy(gameObject));
+            sequence.SetUpdate(true);
+        }
     }
 }
