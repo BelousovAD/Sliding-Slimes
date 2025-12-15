cf63b911f1e1a4c76c09882186669c51605b75e0
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-12-15 21:43:59 +0800

    Добавлены все звуки

--- Assets/Scripts/Audio/AudioClipKey.cs
+++ Assets/Scripts/Audio/AudioClipKey.cs
@@ -2,16 +2,16 @@ namespace Audio
 {
     public enum AudioClipKey
     {
-        Slime,
-        Portal,
-        Travelator,
-        LuckyBlock,
-        Hourglass,
-        Megaphone,
-        Lightning,
-        Click,
-        Defeat,
-        Victory,
-        Background,
+        Slime = 0,
+        Portal = 1,
+        Travelator = 2,
+        LuckyBlock = 3,
+        Hourglass = 4,
+        Megaphone = 5,
+        Lightning = 6,
+        Click = 7,
+        Defeat = 8,
+        Victory = 9,
+        Background = 10,
     }
 }
