3fd215f7e827553ae93c1169384a24846668e5ca
Aleksey Belousov <ya.belousow-lesha2015@yandex.ru>
2025-12-19 00:16:56 +0800

    Заменён текст кнопки языка на иконки с флагами

---
+++ Assets/Scripts/Localization/SwitchLanguageButton.cs
@@ -0,0 +1,57 @@
+namespace Localization
+{
+    using System;
+    using Bootstrap;
+    using Common;
+    using Reflex.Attributes;
+    using Savvy.Interfaces;
+    using UnityEngine;
+
+    internal class SwitchLanguageButton : AbstractButton
+    {
+        [SerializeField] private LocalizationSettings _settings;
+
+        private int _index;
+        private SavvyServicesProvider _services;
+
+        public event Action LanguageIndexChanged;
+
+        public int Index
+        {
+            get
+            {
+                return _index;
+            }
+
+            private set
+            {
+                if (value != _index)
+                {
+                    _index = value;
+                    LanguageIndexChanged?.Invoke();
+                }
+            }
+        }
+
+        [Inject]
+        private void Initialize(SavvyServicesProvider servicesProvider)
+        {
+            _services = servicesProvider;
+            SystemLanguage currentLanguage = _services.Localisation.GetLanguage();
+
+            for (int i = 0; i < _settings.TranslationsData.Length; i++)
+            {
+                if (_settings.TranslationsData[i].SystemLanguage == currentLanguage)
+                {
+                    Index = i;
+                }
+            }
+        }
+
+        protected override void HandleClick()
+        {
+            Index = (Index + 1) % _settings.TranslationsData.Length;
+            _services.Localisation.SetLanguage(_settings.TranslationsData[Index].SystemLanguage);
+        }
+    }
+}
